c51ef9004a9126a43d27ee204fc666fe4933f148
krzszk96 <krzszk96@gmail.com>
2018-10-25 17:11:54 +0200

    check letters added

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,6 @@ function takeWord() {
     for (i = 0; i < x.length; i++) {
         word = word + x.elements[i].value;
     }
-    //console.log(txt);
     localStorage.setItem("word", word);
     document.getElementById("typedword").innerHTML = localStorage.getItem("word");
 }
@@ -22,17 +21,20 @@ function takeLetter() {
     localStorage.setItem("letter", letter);
     document.getElementById("typedletter").innerHTML = localStorage.getItem("letter");
 }
+function letterCheck(){
+  var y = localStorage.getItem("word");
+  var z = localStorage.getItem("letter");
 
+  for (i = 0; i < y.length; i++) {
+    if((y.charAt(i))==(z.charAt(0)))
+      console.log("masz literke!" + (i+1));
+  }
+}
 
 function guessCount(){
   guess = guess - 1;
   if(guess<=0){console.log("game over")}
   else {
     console.log(guess);
-
   }
-}
-
-function letterCheck(){
-
 }
